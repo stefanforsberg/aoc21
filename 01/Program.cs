3f1a085b4b9feb895b377f2b2bd45d423834dc9d
stefan.forsberg <cykelkatalogen@popkram.com>
2021-12-02 20:16:35 +0100

    Initial commit

---
+++ 01/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = System
+                .IO
+                .File
+                .ReadAllLines("input.txt")
+                .Select(x => int.Parse(x));
+
+
+            var increase = input
+                .Where( (d, i) => i == 0 ? false : d > input.ElementAt(i-1))
+                .Count();
+
+            // var increase = 0;
+
+            // for(var i = 1; i < input.Count(); i++) {
+            //     if(input.ElementAt(i) > input.ElementAt(i-1))
+            //     {
+            //         increase++;
+            //     }
+            // }
+
+            Console.WriteLine("Increased: " + increase);
+
+            var increase3Measurement = 0;
+
+            for(var i = 1; i < input.Count()-2; i++) {
+                // var g1 = input.ElementAt(i-1) + input.ElementAt(i) + input.ElementAt(i+1);
+                // var g2 = input.ElementAt(i) + input.ElementAt(i+1) + input.ElementAt(i+2);
+
+                // i and i+1 cancel each other
+                var g1 = input.ElementAt(i-1);
+                var g2 = input.ElementAt(i+2);
+                
+                if(g2> g1)
+                {
+                    increase3Measurement++;
+                }
+            }
+
+            Console.WriteLine("Increased with three-measurement sliding window: " + increase3Measurement);
+        }
+    }
+}
---
+++ 02/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace _02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            p1();
+            p2();
+           
+        }
+
+        static void p1() 
+        {
+            var x = 0;
+            var y = 0;
+
+            System
+                .IO
+                .File
+                .ReadAllLines("input.txt")
+                .ToList()
+                .ForEach(r => { 
+                    var row = r.Split(" ");
+                    var value = int.Parse(row[1]);
+
+                    switch(row[0]) 
+                    {
+                        case "forward": x += value; break;
+                        case "up": y -= value; break;
+                        case "down": y += value; break;
+                    }
+                });
+
+            Console.WriteLine($"Horizontal: {x}, depth: {y}, Multiplied: {x*y}");
+        }
+
+        static void p2() 
+        {
+            var input = System
+                .IO
+                .File
+                .ReadAllLines("input.txt")
+                .Select(x => { 
+                    var row = x.Split(" ");
+
+                    return new {
+                        Cmd = row[0],
+                        Value = int.Parse(row[1])
+                    };
+                });
+
+            var x = 0;
+            var y = 0;
+            var aim = 0;
+
+            foreach(var i in input) 
+            {
+                if(i.Cmd == "forward") 
+                {
+                    x += i.Value;
+                    y += aim*i.Value;
+                }
+                
+                if(i.Cmd == "up") 
+                {
+                    aim -= i.Value;
+                }
+                
+                if(i.Cmd == "down") 
+                {
+                    aim += i.Value;
+                }
+            }
+
+            Console.WriteLine($"Horizontal: {x}, depth: {y}, aim: {aim}. Multiplied: {x*y}");
+        }
+
+        
+    }
+}
