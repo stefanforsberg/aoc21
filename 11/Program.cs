e5339a7dd7fa589cacf3636cfafb3789010385df
stefan.forsberg <cykelkatalogen@popkram.com>
2021-12-12 10:00:55 +0100

    11,12

--- 11/Program.cs
+++ 11/Program.cs
@@ -1,25 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
-namespace _11
+var input = new List<List<int>>();
+
+P1();
+P2();
+
+void P1()
 {
-    class Program
+    Parse();
+
+    var totalFlashed = 0;
+
+    for (var s = 0; s < 100; s++)
     {
+        totalFlashed += Step(s);
+    }
 
-        static void Main(string[] args)
+    Console.WriteLine("Total flashes: " + totalFlashed);
+}
+
+void P2()
+{
+    Parse();
+
+    var flashedForStep = 0;
+
+    var s = 0;
+
+    do
+    {
+        flashedForStep = Step(s);
+        s++;
+    } while (flashedForStep != 100);
+
+    Console.WriteLine("All flashed on: " + s);
+}
+
+void Parse()
+{
+    input = File
+        .ReadAllLines("input.txt")
+        .Select(x => Enumerable.Range(0, x.Length).Select(y => int.Parse(x[y].ToString())).ToList())
+        .ToList();
+}
+
+int Step(int s)
+{
+    for (var y = 0; y < input.Count; y++)
+    {
+        for (var x = 0; x < input[y].Count; x++)
         {
-            p1();
+            IncFlash(x, y);
         }
+    }
 
-        static void p1()
+    var numberOfFlashesForTurn = 0;
+
+    for (var y = 0; y < input.Count; y++)
+    {
+        for (var x = 0; x < input[y].Count; x++)
         {
-            var input = System
-                .IO
-                .File
-                .ReadAllLines("input.txt")
-                .ToList();
+            if (input[y][x] == -1)
+            {
+                input[y][x] = 0;
 
+                numberOfFlashesForTurn++;
+            }
+        }
+    }
+
+    return numberOfFlashesForTurn;
+}
+
+void IncFlash(int x, int y)
+{
+
+    if (x < 0 || x > input.Count - 1 || y < 0 || y > input[0].Count - 1)
+    {
+        return;
+    }
+
+    if (input[y][x] == -1)
+    {
+        return;
+    }
+
+    input[y][x] += 1;
+
+    var flashed = input[y][x] > 9;
+
+    if (flashed)
+    {
+        input[y][x] = -1;
+
+        foreach (var cy in new[] { -1, 0, 1 })
+        {
+            foreach (var cx in new[] { -1, 0, 1 })
+            {
+                IncFlash(x + cx, y + cy);
+            }
         }
     }
 }
---
+++ 12/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var input = new List<(string, string)>();
+
+var s = new System.Diagnostics.Stopwatch();
+s.Start();
+
+P1();
+P2();
+
+Console.WriteLine(s.Elapsed.TotalSeconds);
+
+void P1()
+{
+    Parse();
+
+    var start = input.Where(x => x.Item1 == "start");
+
+    var paths = new List<Path>();
+    var endedPaths = new List<Path>();
+
+    paths.Add(new Path(new[] { "start" }.ToList()));
+
+    do
+    {
+        var path = paths.First();
+
+        var possible = PossibleVisits(path.LatestPoint());
+
+        foreach (var otherpossible in possible.Skip(1))
+        {
+            paths.Add(Path.NewPath(path, otherpossible));
+        }
+
+        path.Add(possible.First());
+
+        endedPaths.AddRange(paths.Where(p => p.IsEnded()));
+
+        paths = paths.Where(p => !p.IsEnded()).ToList();
+
+    } while (paths.Count() > 0);
+
+    var ending = endedPaths.Where(p => p.Visited.Contains("end"));
+
+    Console.WriteLine("Ending cave: " + ending.Count());
+}
+
+List<string> AddToPath(string path, string pos)
+{
+    var newPaths = new List<string>();
+    foreach (var pv in PossibleVisits(pos))
+    {
+        newPaths.Add(path + "->" + pv);
+    }
+
+    return newPaths;
+}
+
+List<string> PossibleVisits(string pos)
+{
+    var f = input
+        .Where(i => i.Item1 == pos)
+        .Select(x => x.Item2);
+
+    var to = input
+        .Where(i => i.Item2 == pos)
+        .Select(x => x.Item1);
+
+    return f.Union(to).Where(x => x != "start").ToList();
+}
+
+
+
+void P2()
+{
+}
+
+void Parse()
+{
+    input = File
+        .ReadAllLines("input.txt")
+        .Select(x => x.Split("-"))
+        .Select(x => (from: x[0], to: x[1]))
+        .ToList();
+}
+
+public class Path
+{
+    private bool ended;
+    public List<string> Visited;
+
+    public bool HasBeenVisistedTwice = false;
+
+    public static Path NewPath(Path path, string to)
+    {
+        var p = new Path(new List<string>(path.Visited));
+        p.HasBeenVisistedTwice = path.HasBeenVisistedTwice;
+        p.Add(to);
+        return p;
+    }
+
+    public Path(List<string> path)
+    {
+        Visited = path;
+    }
+
+    public void Add(string cave)
+    {
+        Visited.Add(cave);
+
+
+        SetIsEnded(cave);
+    }
+
+    public override string ToString()
+    {
+        return string.Join("->", Visited);
+    }
+
+    public string LatestPoint()
+    {
+        return Visited.Last();
+    }
+
+    public bool IsEnded()
+    {
+        return ended;
+    }
+
+    private void SetIsEnded(string cave)
+    {
+        if (cave == "end")
+        {
+            ended = true;
+            return;
+        }
+
+        if (cave.All(char.IsLower))
+        {
+            var visitsForCave = Visited.Count(x => x == cave);
+            if (visitsForCave > 2)
+            {
+                ended = true;
+                return;
+            }
+
+            if (visitsForCave == 2)
+            {
+                if (HasBeenVisistedTwice)
+                {
+                    ended = true;
+                    return;
+                }
+
+                HasBeenVisistedTwice = true;
+
+                // ended = Visited.Where(x => x.All(char.IsLower)).GroupBy(x => x).Select(x => x.Count()).OrderByDescending(x => x).Take(2).Sum() > 3;
+
+                // ended = Visited.Where(x => x.All(char.IsLower)).Where(x => x!= cave).GroupBy(x => x).Any(x => x.Count() > 1);
+            }
+        }
+
+        // ended = Visited.Where(x => x.All(char.IsLower)).GroupBy(x => x).Any(x => x.Count() > 1);
+
+
+        // var smallCaves = Visited.Where(x => x.All(char.IsLower)).GroupBy(x => x).Select(x => x.Count()).OrderByDescending(x => x).Take(2).Sum() > 3;
+        // ended = Visited.Any(x => x == "end") || smallCaves;
+
+        // // p1
+        // ended = Visited.Any(x => x == "end") || Visited.Where(x => x.All(char.IsLower)).GroupBy(x => x).Any(x => x.Count() > 1); 
+    }
+}
