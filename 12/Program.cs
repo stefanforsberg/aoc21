349e46c5401cbb18c9ed416c3e63d1bd754ad7bf
stefan.forsberg <cykelkatalogen@popkram.com>
2021-12-13 05:45:36 +0100

    13

--- 12/Program.cs
+++ 12/Program.cs
@@ -9,16 +9,19 @@ var input = File
     .Select(x => (from: x[0], to: x[1]))
     .ToList();
 
-var possibleMoves = input.Select(x => x.from).Concat(input.Select(x => x.to))
+var possibleMoves = input
+    .Select(x => x.from)
+    .Concat(input.Select(x => x.to))
     .Distinct()
-    .ToDictionary(x => x, x => {
-         var from = input
-            .Where(i => i.Item1 == x)
-            .Select(x => x.Item2);
+    .ToDictionary(x => x, x =>
+    {
+        var from = input
+           .Where(i => i.from == x)
+           .Select(x => x.to);
 
         var to = input
-            .Where(i => i.Item2 == x)
-            .Select(x => x.Item1);
+            .Where(i => i.to == x)
+            .Select(x => x.from);
 
         return from.Union(to).Where(x => x != "start").ToList();
     });
@@ -32,8 +35,8 @@ Console.WriteLine(s.Elapsed.TotalSeconds);
 
 void P2()
 {
-    var paths = new Queue<string[]>();
-    paths.Enqueue(new[] {"start"});
+    var paths = new Queue<(string current, Dictionary<string, int> counter)>();
+    paths.Enqueue(("start", new Dictionary<string, int>()));
 
     var totalPaths = 0;
 
@@ -41,49 +44,40 @@ void P2()
     {
         var path = paths.Dequeue();
 
-        var possible = possibleMoves[path.Last()];
+        var possible = possibleMoves[path.current];
 
         foreach (var moveTo in possible)
         {
-            if(moveTo == "end")
+            if (moveTo == "end")
             {
                 totalPaths++;
                 continue;
             }
 
-            var newPath = new string[path.Length+1]; 
-            Array.Copy(path, newPath, path.Length);
-            newPath[path.Length] = moveTo;
+            var newPath = new Dictionary<string, int>(path.counter);
 
-            if(!IllegalMoveP2(newPath))
+            if (Char.IsLower(moveTo[0]))
             {
-                paths.Enqueue(newPath);
+                if(!newPath.TryAdd(moveTo, 1))
+                {
+                    newPath[moveTo]++;
+                }
+                
+                // // P1
+                // if(newPath.Any(n => n.Value > 1))
+                // {
+                //     continue;
+                // }
+
+                if (newPath[moveTo] > 2 || newPath.Count(n => n.Value > 1) > 1)
+                {
+                    continue;
+                }
             }
+
+            paths.Enqueue((moveTo, newPath));
         }
     } while (paths.Count() > 0);
 
     Console.WriteLine("Total number of paths: " + totalPaths);
-}
-
-bool IllegalMoveP1(List<string> path)
-{
-    return path.Where(x => x.All(char.IsLower)).GroupBy(x => x).Any(x => x.Count() > 1);
-}
-
-bool IllegalMoveP2(string[] path)
-{
-    var newCave = path.Last();
-
-    if (!newCave.All(char.IsLower))
-    {
-        return false;
-    }
-
-    var c = path.Count(x => x == newCave);
-    
-    if(c <= 1) return false;
-
-    if(c > 2) return true;
-
-    return path.Where(x => x.All(char.IsLower)).GroupBy(x => x).Count(x => x.Count() > 1) > 1;
 }
---
+++ 13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var input = File
+    .ReadAllLines("input.txt")
+    .Select(x => x.Split("-"))
+    .Select(x => (from: x[0], to: x[1]))
+    .ToList();
+
+var possibleMoves = input
+    .Select(x => x.from)
+    .Concat(input.Select(x => x.to))
+    .Distinct()
+    .ToDictionary(x => x, x =>
+    {
+        var from = input
+           .Where(i => i.from == x)
+           .Select(x => x.to);
+
+        var to = input
+            .Where(i => i.to == x)
+            .Select(x => x.from);
+
+        return from.Union(to).Where(x => x != "start").ToList();
+    });
+
+var s = new System.Diagnostics.Stopwatch();
+s.Start();
+
+P1();
+
+Console.WriteLine(s.Elapsed.TotalSeconds);
+
+void P1()
+{
+}
