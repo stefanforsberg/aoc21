4f50c29f511a02b56ab876cbcc2728365f642642
stefan.forsberg <cykelkatalogen@popkram.com>
2021-12-16 05:57:21 +0100

    16

---
+++ 16/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+var s = new System.Diagnostics.Stopwatch();
+s.Start();
+
+var input = File
+        .ReadAllLines("input.txt")
+        .Select(x => Enumerable.Range(0, x.Length).Select(y => int.Parse(x[y].ToString())).ToList())
+        .ToList();
+
+Console.WriteLine("Total: " + s.Elapsed.TotalSeconds);
